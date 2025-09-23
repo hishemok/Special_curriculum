363e863b97dfe140e4c9de65aeecbe8d2627898e
Hishem <hishemok@uio.no>
2025-09-23 15:37:24 +0200

    p

---
+++ src/mb_sweetspot.py
@@ -0,0 +1,78 @@
+"""Finding the parameter sweet spot for the many body PMM system"""
+
+import sympy as sp
+import numpy as np
+import matplotlib.pyplot as plt
+from operators import dL, dL_dag, dR, dR_dag
+
+epsL, epsR = sp.symbols('ϵ_L,ϵ_R') #Onsite energies
+t, Delta = sp.symbols('t,Δ') #tunneling and pairing amplit
+U = sp.symbols('U') #Coulomb interaction
+
+def check_eigenvalues():
+    """Compare the analytical and numerical eigenvalues of the Hamiltonian."""
+    H_onsite = epsL * dL_dag * dL + epsR * dR_dag * dR
+    H_tunnel = t * (dL_dag * dR + dR_dag * dL)
+    H_pairing = Delta * (dR * dL + dL_dag * dR_dag)
+    H_coulomb = U * (dL_dag * dL) * (dR_dag * dR)
+
+    H = H_onsite + H_tunnel + H_pairing + H_coulomb
+    H = sp.simplify(H)
+    eigvals = H.eigenvals()
+
+    ### Analytical eigenvalues from block diagonalization
+    S = epsL + epsR + U
+    E_even = [(S + sp.sqrt(S**2 + 4*Delta**2))/2, (S - sp.sqrt(S**2 + 4*Delta**2))/2]
+    E_odd  = [((epsL + epsR) + sp.sqrt((epsL - epsR)**2 + 4*t**2))/2, ((epsL + epsR) - sp.sqrt((epsL - epsR)**2 + 4*t**2))/2]
+
+    ### Check equality
+    all_eigvals = E_even + E_odd
+    for val in all_eigvals:
+        found = any(sp.simplify(val - ev) == 0 for ev in eigvals.keys())
+        assert found, f"Eigenvalue {val} not found in numerical eigenvalues."
+    
+    
+    return E_even, E_odd 
+
+Even, Odd = check_eigenvalues()
+
+#Check parameters for sweet spot
+"""Sweet spot conditions:
+[E_even]_min = [E_odd]_min
+"""
+
+def Compute_U(epsL,epsR,Delta,t):
+    p1 = (Delta**2-t**2 + epsL*epsR)
+    p2 = (epsL + epsR + 2*np.sqrt(t**2 + (epsL - epsR)**2))
+    p3 = 2*(t**2 - epsL*epsR)
+    return p1*p2/p3
+
+eps = .5
+t_val = 1
+
+Delta_vals = np.linspace(0.01, 2, 100)
+Evals = np.zeros((len(Delta_vals), 4), dtype=float)
+
+for i, Delta_val in enumerate(Delta_vals):
+    U_val = Compute_U(eps,eps,Delta_val,t_val)
+    # print(Even[0].subs({epsL:eps, epsR:eps, U:U_val, Delta:Delta_val}))
+    even0 = float(Even[0].subs({epsL:eps, epsR:eps, U:U_val, Delta:Delta_val}).evalf())
+    even1 = float(Even[1].subs({epsL:eps, epsR:eps, U:U_val, Delta:Delta_val}).evalf())
+    odd0  = float(Odd[0].subs({epsL:eps, epsR:eps, t:t_val}).evalf())
+    odd1  = float(Odd[1].subs({epsL:eps, epsR:eps, t:t_val}).evalf())
+    
+    Evals[i] = [even0, even1, odd0, odd1]
+
+"""SORT THE EIGENVALUES?"""
+
+plt.figure()
+linestyles = ['-', '--', '-', '--']  # 4 different styles
+labels = [rf'$E_{{even0}}$', rf'$E_{{even1}}$', rf'$E_{{odd0}}$', rf'$E_{{odd1}}$']
+for k in range(Evals.shape[1]): # loop over eigenvalue branches
+    plt.plot(Delta_vals, Evals[:, k], linestyle=linestyles[k], label=labels[k]) 
+plt.xlabel(r'$Δ$ (with $ϵ_L=ϵ_R=0.5, t=1$)')
+plt.ylabel('Eigenvalues')
+plt.title(r'Eigenvalues vs $Δ$ at the Sweet Spot $(ϵ_R=0.5, ϵ_L=0.5, t=1)$')
+plt.legend()
+plt.grid()
+plt.show()
